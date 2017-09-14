c1f1e6937bd048c2549ed4e7eeef09d70bdde163
YukiUeda <ikuyadeu@yahoo.co.jp>
2017-09-14 13:05:33 +0900

    re-fix

--- R/zzz.R
+++ R/zzz.R
@@ -6,17 +6,6 @@ request_handlers <- list(
 notification_handlers <- list(
     initialized = on_initialized,
     exit = on_exit,
-    `textDocument/didOpen` = textDocumentdidOpen,
-    `textDocument/didSave` = textDocumentdidSave
-)
-
-general <- list(
-    initialized = on_initialized,
-    initialize = on_initialize,
-    exit = on_exit
-)
-
-text_document <- list(
     `textDocument/didOpen` = text_document_did_open,
     `textDocument/didSave` = text_document_did_save
 )
