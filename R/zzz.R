3ef30019c935471ef8a8ec690c5ee8dfbf0a85a1
YukiUeda <ikuyadeu@yahoo.co.jp>
2017-09-14 13:05:09 +0900

    return handlers

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,15 @@
+request_handlers <- list(
+    initialize = on_initialize
+)
+
+
+notification_handlers <- list(
+    initialized = on_initialized,
+    exit = on_exit,
+    `textDocument/didOpen` = textDocumentdidOpen,
+    `textDocument/didSave` = textDocumentdidSave
+)
+
 general <- list(
     initialized = on_initialized,
     initialize = on_initialize,
