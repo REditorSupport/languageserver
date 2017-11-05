2d75e40914dd5c845247b93bd46763b44432cc82
Randy Lai <randy.cs.lai@gmail.com>
2017-11-05 02:45:31 -0500

    enhanced completion feature

--- R/completion.R
+++ R/completion.R
@@ -19,6 +19,48 @@ CompletionItemKind <- list(
     Reference = 18
 )
 
+find_token <- function(line) {
+    utils:::.assignLinebuffer(line)
+    utils:::.assignEnd(nchar(line))
+    token <- utils:::.guessTokenFromLine()
+    logger$info("token: ", token)
+    token
+}
+
+default_completion <- function(token) {
+    logger$info("completing: ", token)
+    # token <- utils:::.guessTokenFromLine(update = FALSE)
+    utils:::.completeToken()
+    comps <- utils:::.retrieveCompletions()
+    comps <- stringr::str_replace(comps, "=", " = ")
+    comps <- stringr::str_replace(comps, "<-", " <- ")
+
+    logger$info("comps: ", comps)
+
+    completions <- list()
+    for (i in seq_along(comps)) {
+        completions[[i]] <- list(label = comps[i])
+    }
+
+    completions
+}
+
+
+package_completion <- function(token) {
+    installed_packages <- rownames(installed.packages())
+    completions <- list()
+
+    for (package in installed_packages) {
+        if (startsWith(package, token)) {
+            completions <- append(completions, list(list(
+                label = paste0(package, "::"),
+                kind = CompletionItemKind$Module
+            )))
+        }
+    }
+    completions
+}
+
 completion_reply <- function(id, document, position) {
     line <- document_line(document, position$line + 1)
     character <- position$character
@@ -26,21 +68,24 @@ completion_reply <- function(id, document, position) {
         line <- stringr::str_sub(line, end = character)
     }
 
-    logger$info("completing: ", line)
-    utils:::.assignLinebuffer(line)
-    utils:::.assignEnd(nchar(line))
-    token <- utils:::.guessTokenFromLine()
-    # token <- utils:::.guessTokenFromLine(update = FALSE)
-    utils:::.completeToken()
-    logger$info("token: ", token)
-    comps <- utils:::.retrieveCompletions()
+    token <- find_token(line)
+
+    providers <- c(
+        default_completion,
+        package_completion
+    )
 
     completions <- list()
-    for (i in seq_along(comps)) {
-        completions[[i]] <- list(label = comps[i])
+
+    for (provider in providers) {
+        provider_completion <- provider(token)
+        if (length(provider_completion) > 0) {
+            completions <- append(completions, provider_completion)
+        }
     }
 
     logger$info("completions: ", completions)
+
 
     Response$new(
         id,
