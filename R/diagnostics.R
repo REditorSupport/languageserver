2f63d2769b067dc2cb0838d2a1dba216e7eaa27c
Randy Lai <randy.cs.lai@gmail.com>
2019-03-07 03:04:17 -0500

    disable home directory config for now

--- R/diagnostics.R
+++ R/diagnostics.R
@@ -71,11 +71,13 @@ find_config <- function(filename) {
     if (isTRUE(file.exists(linter_config))) {
         return(linter_config)
     }
-    home_dir <- Sys.getenv("HOME", unset = "~")
-    linter_config <- file.path(home_dir, linter_file)
-    if (isTRUE(file.exists(linter_config))) {
-        return(linter_config)
-    }
+    # this version of find_config has not released
+
+    # home_dir <- Sys.getenv("HOME", unset = "~")
+    # linter_config <- file.path(home_dir, linter_file)
+    # if (isTRUE(file.exists(linter_config))) {
+    #     return(linter_config)
+    # }
     NULL
 }
 
