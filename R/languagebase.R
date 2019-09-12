c9f0303632c1beb9e3e02358c731c135d25c8d71
Randy Lai <randy.cs.lai@gmail.com>
2019-09-11 20:46:05 -0700

    refactor common code of language server and language client

---
+++ R/languagebase.R
@@ -0,0 +1,121 @@
+# Parent class of LanguageServer and LanguageClient, containing
+# some of the common functions
+
+LanguageBase <- R6::R6Class("LanguageBase",
+    private = list(
+        ticket = 0
+    ),
+    public = list(
+        request_handlers = NULL,
+        notification_handlers = NULL,
+        request_callbacks = NULL,
+
+        get_ticket = function() {
+            private$ticket <- private$ticket + 1
+            private$ticket
+        },
+
+        request = function(method, params) {
+            Request$new(
+                self$get_ticket(),
+                method,
+                params
+            )
+        },
+
+        deliver = function(message, callback = NULL) {
+            if (is.null(message)) {
+                return(NULL)
+            }
+            logger$info("deliver: ", class(message))
+            method <- message$method
+            if (!is.null(method)) {
+                logger$info("method: ", method)
+            }
+            self$write_text(message$format())
+            if (inherits(message, "Request") && !is.null(callback)) {
+                id <- message$id
+                self$request_callbacks$set(as.character(id), callback)
+            }
+        },
+
+        handle_raw = function(data) {
+            payload <- tryCatch(
+                jsonlite::fromJSON(data, simplifyVector = FALSE),
+                error = function(e) e
+            )
+            if (inherits(payload, "error")) {
+                logger$error("error handling json: ", payload)
+                return(NULL)
+            }
+            pl_names <- names(payload)
+            logger$info("received payload.")
+            if ("id" %in% pl_names && "method" %in% pl_names) {
+                self$handle_request(payload)
+            } else if ("method" %in% pl_names) {
+                self$handle_notification(payload)
+            } else if ("id" %in% pl_names) {
+                self$handle_response(payload)
+            } else {
+                logger$error("unknown message")
+            }
+        },
+
+        handle_request = function(request) {
+            id <- request$id
+            method <- request$method
+            params <- request$params
+            if (method %in% names(self$request_handlers)) {
+                logger$info("handling request: ", method)
+                tryCatch({
+                    dispatch <- self$request_handlers[[method]]
+                    dispatch(self, id, params)
+                },
+                error = function(e) {
+                    logger$error("internal error: ", e)
+                    self$deliver(ResponseErrorMessage$new(id, "InternalError", to_string(e)))
+                }
+                )
+            } else {
+                logger$error("unknown request: ", method)
+                self$deliver(ResponseErrorMessage$new(
+                    id, "MethodNotFound", paste0("unknown request ", method)
+                ))
+            }
+        },
+
+        handle_notification = function(notification) {
+            method <- notification$method
+            params <- notification$params
+            if (method %in% names(self$notification_handlers)) {
+                logger$info("handling notification: ", method)
+                tryCatch({
+                    dispatch <- self$notification_handlers[[method]]
+                    dispatch(self, params)
+                },
+                error = function(e) {
+                    logger$error("internal error: ", e)
+                }
+                )
+            } else {
+                logger$error("unknown notification: ", method)
+            }
+        },
+
+        handle_response = function(response) {
+            id <- response$id
+            callback <- tryCatch(
+                self$request_callbacks$pop(as.character(id)),
+                error = function(e) NULL
+            )
+            if ("error" %in% names(response)) {
+                logger$info("got an error: ", response$error)
+            } else if (!is.null(callback)) {
+                tryCatch(
+                    callback(self, response$result),
+                    error = function(e) logger$info("callback error: ", e)
+                )
+            }
+        }
+    )
+)
--- R/languageclient.R
+++ R/languageclient.R
@@ -1,57 +1,27 @@
 # for testing purpose
 
 LanguageClient <- R6::R6Class("LanguageClient",
-    private = list(
-        ticket = 0
-    ),
+    inherit = LanguageBase,
     public = list(
         process = NULL,
         rootUri = NULL,
         ClientCapabilities = NULL,
         ServerCapabilities = NULL,
 
-        request_handlers = NULL,
-        notification_handlers = NULL,
-        response_handlers = NULL,
-
         initialize = function(cmd, args) {
             self$process <- processx::process$new(cmd, args,
                 stdin = "|", stdout = "|", stderr = "|", supervise = TRUE)
             self$register_handlers()
-            self$response_handlers <- collections::Dict$new()
-        },
-
-        get_ticket = function() {
-            private$ticket <- private$ticket + 1
-            private$ticket
-        },
-
-        deliver = function(message, callback = NULL) {
-            if (!is.null(message)) {
-                self$process$write_input(message$format())
-                logger$info("deliver: ", class(message))
-                method <- message$method
-                if (!is.null(method)) {
-                    logger$info("method: ", method)
-                }
-                if (inherits(message, "Request") && !is.null(callback)) {
-                    id <- message$id
-                    self$response_handlers$set(as.character(id), callback)
-                }
-            }
-        },
-
-        request = function(method, params) {
-            Request$new(
-                self$get_ticket(),
-                method,
-                params
-            )
+            self$request_callbacks <- collections::Dict$new()
         },
 
         check_connection = function() {
             if (!self$process$is_alive())
                 stop("Server is dead.")
+        },
+
+        write_text = function(text) {
+            self$process$write_input(text)
         },
 
         read_one_output_line = function() {
@@ -74,7 +44,6 @@ LanguageClient <- R6::R6Class("LanguageClient",
             empty_line <- self$read_one_output_line()
             while (length(empty_line) == 0) {
                 empty_line <- self$read_one_output_line()
-                empty_line
                 Sys.sleep(0.01)
             }
             if (nchar(empty_line) > 0) {
@@ -109,36 +78,6 @@ LanguageClient <- R6::R6Class("LanguageClient",
             data
         },
 
-        handle_raw = function(data) {
-            payload <- tryCatch(
-                jsonlite::fromJSON(data, simplifyVector = FALSE),
-                error = function(e) e)
-            if (inherits(payload, "error")) {
-                logger$error("error handling json: ", payload)
-                return(NULL)
-            }
-            pl_names <- names(payload)
-            logger$info("received payload.")
-
-            if ("result" %in% pl_names) {
-                self$handle_response(payload)
-            } else {
-                logger$error("got unexpected message")
-            }
-        },
-
-        handle_response = function(response) {
-            id <- response$id
-            callback <- self$response_handlers$pop(as.character(id))
-            if ("error" %in% names(response)) {
-                logger$info("got an error: ", response$error)
-            } else {
-                tryCatch(
-                    callback(self, response$result),
-                    error = function(e) logger$info("callback error: ", e))
-            }
-        },
-
         welcome = function() {
             self$deliver(
                 self$request(
@@ -162,7 +101,11 @@ LanguageClient <- R6::R6Class("LanguageClient",
 
         stop = function() {
             self$process$kill()
-            self$response_handlers <- NULL
+            self$request_callbacks <- NULL
+        },
+
+        run = function() {
+            # placeholder
         }
     )
 )
