ce5fec2977b16a01b5dc162e0ca36886b3d89ee0
Randy Lai <randy.cs.lai@gmail.com>
2019-10-29 13:51:46 -0700

    improve code_point_to_unit test

--- tests/testthat/test-codeunits.R
+++ tests/testthat/test-codeunits.R
@@ -7,5 +7,5 @@ test_that("Code units are correctly calculated", {
     expect_equal(ncodeunit("佢"), 1)
     expect_equal(ncodeunit("𠍲"), 2)
     expect_equal(ncodeunit(c("佢", "𠍲")), c(1, 2))
-    expect_equal(code_point_to_unit("a𐐀c", 0:5), c(0, 1, NA, 2, 3, NA))
+    expect_equal(code_point_to_unit("a𐐀c", c(0:5, Inf)), c(0, 1, NA, 2, 3, NA, 3))
 })
