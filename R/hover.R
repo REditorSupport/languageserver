199273ac71a58acf368df70d5162328fab413622
Kun Ren <renkun@outlook.com>
2019-11-20 17:18:34 +0800

    Fix hover contents

--- R/hover.R
+++ R/hover.R
@@ -18,8 +18,8 @@ hover_reply <- function(id, uri, workspace, document, position) {
         logger$info("sig: ", sig)
         if (!is.null(sig)) {
             sig <- trimws(gsub("function\\s*", token_result$token, sig))
+            contents <- sprintf("```r\n%s\n```", sig)
         }
-        contents <- sprintf("```r\n%s\n```", sig)
     }
 
     if (is.null(contents)) {
