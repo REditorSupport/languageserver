78c77997e432a935d1f8c8165e095bd93ee942c7
Kun Ren <renkun@outlook.com>
2019-11-21 07:57:10 +0800

    Add tests for hover on user function

--- tests/testthat/test-hover.R
+++ tests/testthat/test-hover.R
@@ -24,3 +24,23 @@ test_that("Simple hover works", {
     expect_true(stringr::str_detect(result$contents[1], "path_real"))
     expect_equal(result$range$end$character, 13)
 })
+
+test_that("Hover on user function works", {
+    skip_on_cran()
+    client <- language_client()
+
+    withr::local_tempfile(c("temp_file"), fileext = ".R")
+    writeLines(
+        c(
+            "test1 <- function(x, y) x + 1",
+            "test1"
+        ),
+        temp_file)
+
+    client %>% did_save(temp_file)
+
+    result <- client %>% respond_hover(temp_file, c(1, 3))
+    expect_length(result$contents, 1)
+    expect_equal(result$contents[1], "```r\ntest1(x, y)\n```")
+    expect_equal(result$range$end$character, 5)
+})
