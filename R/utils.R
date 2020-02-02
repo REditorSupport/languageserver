9778d3bd1e5e76b56fbdf317e3aabe3bc6c31bca
Kun Ren <renkun@outlook.com>
2020-02-03 05:54:30 +0800

    Refine print.errorWithStack and re-tab

--- R/utils.R
+++ R/utils.R
@@ -2,35 +2,38 @@
 #'
 #' @keywords internal
 merge_list <- function(x, y) {
-  x[names(y)] <- y
-  x
+    x[names(y)] <- y
+    x
 }
 
 #' tryCatch with stack captured
 #'
 #' @keywords internal
 tryCatchStack <- function(expr, ...) {
-  expr <- substitute(expr)
-  env <- parent.frame()
-  capture_calls <- function(e) {
-    calls <- sys.calls()
-    ncalls <- length(calls)
-    e$calls <- c(calls[-c(seq_len(frame + 7), ncalls - 1, ncalls)], e$call)
-    class(e) <- c("errorWithStack", class(e))
-    signalCondition(e)
-  }
-  frame <- sys.nframe()
-  tryCatch(withCallingHandlers(eval(expr, env), error = capture_calls), ...)
+    expr <- substitute(expr)
+    env <- parent.frame()
+    capture_calls <- function(e) {
+        calls <- sys.calls()
+        ncalls <- length(calls)
+        e$calls <- c(calls[-c(seq_len(frame + 7), ncalls - 1, ncalls)], e$call)
+        class(e) <- c("errorWithStack", class(e))
+        signalCondition(e)
+    }
+    frame <- sys.nframe()
+    tryCatch(withCallingHandlers(eval(expr, env), error = capture_calls), ...)
 }
 
 print.errorWithStack <- function(x, ...) {
-  cat("Error: ", conditionMessage(x), "\n", sep = "")
-  cat("Stack trace:\n")
-  rev_calls <- rev(x$calls)
-  for (i in seq_along(rev_calls)) {
-    cat(i, ": ", sep = "")
-    print(rev_calls[[i]])
-  }
+    cat("Error: ", conditionMessage(x), "\n", sep = "")
+    if (length(x$calls)) {
+        cat("Stack trace:\n")
+        rev_calls <- rev(x$calls)
+        for (i in seq_along(rev_calls)) {
+            cat(i, ": ", sep = "")
+            print(rev_calls[[i]])
+        }
+    }
+    invisible(x)
 }
 
 capture_print <- function(x) {
