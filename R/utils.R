90394ac0030ae75634076e21fcd092fab565a1fc
Kun Ren <renkun@outlook.com>
2020-02-02 21:58:22 +0800

    Use print.errorWithStack

--- R/utils.R
+++ R/utils.R
@@ -16,11 +16,25 @@ tryCatchStack <- function(expr, ...) {
     calls <- sys.calls()
     ncalls <- length(calls)
     e$calls <- c(calls[-c(seq_len(frame + 7), ncalls - 1, ncalls)], e$call)
-    e$calls <- lapply(e$calls, deparse)
+    class(e) <- c("errorWithStack", class(e))
     signalCondition(e)
   }
   frame <- sys.nframe()
   tryCatch(withCallingHandlers(eval(expr, env), error = capture_calls), ...)
+}
+
+print.errorWithStack <- function(x, ...) {
+  cat("Error: ", conditionMessage(x), "\n", sep = "")
+  cat("Stack trace:\n")
+  rev_calls <- rev(x$calls)
+  for (i in seq_along(rev_calls)) {
+    cat(i, ": ", sep = "")
+    print(rev_calls[[i]])
+  }
+}
+
+capture_print <- function(x) {
+    paste0(utils::capture.output(print(x)), collapse = "\n")
 }
 
 #' Paths and uris
