341b00a637add78e41ebd94c48207baaaede1711
Randy Lai <randy.cs.lai@gmail.com>
2020-02-09 09:38:38 -0800

    test Rmarkdown formatting

--- tests/testthat/test-rmarkdown.R
+++ tests/testthat/test-rmarkdown.R
@@ -108,3 +108,28 @@ test_that("Go to Definition works for in Rmarkdown", {
 
     expect_equal(length(result), 0)
 })
+
+
+test_that("Formatting works for in Rmarkdown", {
+    skip_on_cran()
+    client <- language_client()
+
+    withr::local_tempfile(c("single_file"), fileext = ".Rmd")
+    writeLines(
+        c(
+            "```{r}",
+            "my_fn= function(x) {x + 1; x}",
+            "```"
+        ),
+        single_file)
+
+    client %>% did_save(single_file)
+
+    # first query a known function to make sure the file is processed
+    result <- client %>% respond_formatting(single_file)
+
+    expect_length(result, 1)
+    expect_equal(result[[1]]$range$start, list(line = 1, character = 0))
+    expect_equal(result[[1]]$range$end, list(line = 1, character = 29))
+    expect_equal(result[[1]]$newText, "my_fn <- function(x) {\n    x + 1\n    x\n}")
+})
