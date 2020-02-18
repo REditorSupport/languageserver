222bd60e61c293f5388359ee3277f1ea4fe427eb
Kun Ren <renkun@outlook.com>
2020-02-18 10:56:38 +0800

    Update formatting test cases

--- tests/testthat/test-formatting.R
+++ tests/testthat/test-formatting.R
@@ -5,15 +5,24 @@ test_that("Formatting document works", {
     client <- language_client()
 
     withr::local_tempfile(c("temp_file"), fileext = ".R")
-    writeLines(c("my_fn <- function(x) {  x + 1 }"), temp_file)
+    writeLines(c(
+        "my_fn<-function(x){", 
+        "f(x+1,x-1)+x", 
+        "}"
+    ), temp_file)
 
     client %>% did_save(temp_file)
 
     result <- client %>% respond_formatting(temp_file)
 
     expect_length(result, 1)
-    lines <- result[[1]]$newText %>% strsplit("\n") %>% extract2(1)
+    lines <- strsplit(result[[1]]$newText, "\n")[[1]]
     expect_length(lines, 3)
+    expect_equal(lines, c(
+        "my_fn <- function(x) {",
+        "    f(x + 1, x - 1) + x",
+        "}"
+    ))
 })
 
 
@@ -22,14 +31,19 @@ test_that("Formatting selection works for complete line", {
     client <- language_client()
 
     withr::local_tempfile(c("temp_file"), fileext = ".R")
-    writeLines(c("my_fn <- function(x) {", "    y =x+ 1", "    y+3", "}"), temp_file)
+    writeLines(c(
+        "my_fn <- function(x) {",
+        "    y =x+ 1",
+        "    y+3",
+        "}"
+    ), temp_file)
 
     client %>% did_save(temp_file)
 
     result <- client %>% respond_range_formatting(temp_file, c(1, 0), c(2, 7))
 
     expect_length(result, 1)
-    lines <- result[[1]]$newText %>% strsplit("\n") %>% extract2(1)
+    lines <- strsplit(result[[1]]$newText, "\n")[[1]]
     expect_equal(lines[1], "    y <- x + 1")
     expect_equal(lines[2], "    y + 3")
 })
@@ -40,14 +54,19 @@ test_that("Formatting selection works for partial line", {
     client <- language_client()
 
     withr::local_tempfile(c("temp_file"), fileext = ".R")
-    writeLines(c("my_fn <- function(x) {", "    y =x+ 1", "    y+3", "}"), temp_file)
+    writeLines(c(
+        "my_fn <- function(x) {", 
+        "    y =x+ 1", 
+        "    y+3", 
+        "}"
+    ), temp_file)
 
     client %>% did_save(temp_file)
 
     result <- client %>% respond_range_formatting(temp_file, c(1, 4), c(2, 7))
 
     expect_length(result, 1)
-    lines <- result[[1]]$newText %>% strsplit("\n") %>% extract2(1)
+    lines <- strsplit(result[[1]]$newText, "\n")[[1]]
     expect_equal(lines[1], "    y = x + 1")
     expect_equal(lines[2], "    y + 3")
 })
@@ -58,12 +77,31 @@ test_that("On type formatting works", {
     client <- language_client()
 
     withr::local_tempfile(c("temp_file"), fileext = ".R")
-    writeLines(c("my_fun <- function(x) { x }"), temp_file)
+    writeLines(c(
+        "my_fn<-function(x){",
+        "f(x+1,x-1)",
+        "data[x,y]",
+        "}"
+    ), temp_file)
 
     client %>% did_save(temp_file)
 
-    result <- client %>% respond_on_type_formatting(temp_file, c(0, 27), "}")
-
+    result <- client %>% respond_on_type_formatting(temp_file, c(1, 10), ")")
     expect_length(result, 1)
-    expect_equal(result[[1]]$newText, "my_fun <- function(x) {\n    x\n}")
+    expect_equal(result[[1]]$newText, "f(x + 1, x - 1)")
+
+    result <- client %>% respond_on_type_formatting(temp_file, c(2, 9), ")")
+    expect_length(result, 1)
+    expect_equal(result[[1]]$newText, "data[x, y]")
+
+    result <- client %>% respond_on_type_formatting(temp_file, c(3, 1), "}")
+    expect_length(result, 1)
+    lines <- strsplit(result[[1]]$newText, "\n")[[1]]
+    expect_length(lines, 4)
+    expect_equal(lines, c(
+        "my_fn <- function(x) {",
+        "    f(x + 1, x - 1)",
+        "    data[x, y]",
+        "}"
+    ))
 })
