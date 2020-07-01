4dae67ac2e9fe9379c79f52746041f11387517e1
Kun Ren <renkun@outlook.com>
2020-07-01 20:16:37 +0800

    Refactor get_document_sections

--- R/symbol.R
+++ R/symbol.R
@@ -28,157 +28,6 @@ SymbolKind <- list(
     TypeParameter = 26
 )
 
-get_r_document_section_symbols <- function(uri, document) {
-    section_symbols <- NULL
-    section_lines <- seq_len(document$nline)
-    section_lines <- section_lines[
-        grep("^\\#+\\s*(.+?)\\s*(\\#{4,}|\\+{4,}|\\-{4,}|\\={4,})\\s*$",
-            document$content[section_lines], perl = TRUE)]
-    logger$info("document sections found: ", length(section_lines))
-    if (length(section_lines)) {
-        section_names <- gsub("^\\#+\\s*(.+?)\\s*(\\#{4,}|\\+{4,}|\\-{4,}|\\={4,})\\s*$",
-            "\\1", document$content[section_lines], perl = TRUE)
-        section_end_lines <- c(section_lines[-1] - 1, document$nline)
-        section_symbols <- .mapply(function(name, start_line, end_line) {
-            symbol_information(
-                name = name,
-                kind = SymbolKind$String,
-                location = list(
-                    uri = uri,
-                    range = range(
-                        start = document$to_lsp_position(row = start_line - 1, col = 0),
-                        end = document$to_lsp_position(row = end_line - 1, col = nchar(document$line(end_line)))
-                    )
-                )
-            )
-        }, list(section_names, section_lines, section_end_lines), NULL)
-    }
-
-    subsection_symbols <- NULL
-    subsection_lines <- seq_len(document$nline)
-    subsection_lines <- subsection_lines[
-        grep("^\\s+\\#+\\s*(.+?)\\s*(\\#{4,}|\\+{4,}|\\-{4,}|\\={4,})\\s*$",
-            document$content[subsection_lines], perl = TRUE)]
-    logger$info("document subsections found: ", length(subsection_lines))
-    if (length(subsection_lines)) {
-        subsection_names <- gsub("^\\s+\\#+\\s*(.+?)\\s*(\\#{4,}|\\+{4,}|\\-{4,}|\\={4,})\\s*$",
-            "\\1", document$content[subsection_lines], perl = TRUE)
-        subsection_symbols <- .mapply(function(name, line) {
-            symbol_information(
-                name = name,
-                kind = SymbolKind$String,
-                location = list(
-                    uri = uri,
-                    range = range(
-                        start = document$to_lsp_position(row = line - 1, col = 0),
-                        end = document$to_lsp_position(row = line - 1, col = nchar(document$line(line)))
-                    )
-                )
-            )
-        }, list(subsection_names, subsection_lines), NULL)
-    }
-
-    c(section_symbols, subsection_symbols)
-}
-
-get_rmd_document_section_symbols <- function(uri, document) {
-    content <- document$content
-    if (length(content) == 0) {
-        return(NULL)
-    }
-
-    block_lines <- grep("^\\s*```", content)
-    if (length(block_lines) %% 2 != 0) {
-        return(NULL)
-    }
-
-    section_lines <- grepl("^#+\\s+\\S+", content)
-    if (grepl("^---\\s*$", content[[1]])) {
-        front_start <- 1L
-        front_end <- 2L
-        while (front_end <= document$nline) {
-            if (grepl("^---\\s*$", content[[front_end]])) {
-                break
-            }
-            front_end <- front_end + 1L
-        }
-        section_lines[seq.int(front_start, front_end)] <- FALSE
-    }
-
-    for (i in seq_len(length(block_lines) / 2)) {
-        section_lines[seq.int(block_lines[[2 * i - 1]], block_lines[[2 * i]])] <- FALSE
-    }
-
-    section_lines <- which(section_lines)
-    section_num <- length(section_lines)
-    section_texts <- content[section_lines]
-    section_hashes <- gsub("^(#+)\\s+.+$", "\\1", section_texts)
-    section_levels <- nchar(section_hashes)
-    section_names <- gsub("^#+\\s+(.+?)(\\s+#+)?\\s*$", "\\1", section_texts, perl = TRUE)
-
-    section_symbols <- lapply(seq_len(section_num), function(i) {
-        start_line <- section_lines[[i]]
-        end_line <- document$nline
-        level <- section_levels[[i]]
-        j <- i + 1
-        while (j <= section_num) {
-            if (section_levels[[j]] <= level) {
-                end_line <- section_lines[[j]] - 1
-                break
-            }
-            j <- j + 1
-        }
-        symbol_information(
-            name = section_names[[i]],
-            kind = SymbolKind$String,
-            location = list(
-                uri = uri,
-                range = range(
-                    start = document$to_lsp_position(row = start_line - 1, col = 0),
-                    end = document$to_lsp_position(row = end_line - 1, col = nchar(document$line(end_line)))
-                )
-            )
-        )
-    })
-
-    unnamed_chunks <- 0
-    chunk_symbols <- lapply(seq_len(length(block_lines) / 2), function(i) {
-        start_line <- block_lines[[2 * i - 1]]
-        end_line <- block_lines[[2 * i]]
-        label <- stringi::stri_match_first_regex(content[[start_line]],
-            "^\\s*```+\\s*\\{[a-zA-Z0-9_]+\\s*(([^,'\"]+)|'(.+)'|\"(.+)\")\\s*(,.+)?\\}\\s*$"
-        )[1, 3:5]
-        name <- label[!is.na(label)]
-
-        if (length(name) == 0) {
-            unnamed_chunks <<- unnamed_chunks + 1
-            name <- sprintf("unnamed-chunk-%d", unnamed_chunks)
-        }
-
-        symbol_information(
-            name = name,
-            kind = SymbolKind$Key,
-            location = list(
-                uri = uri,
-                range = range(
-                    start = document$to_lsp_position(row = start_line - 1, col = 0),
-                    end = document$to_lsp_position(row = end_line - 1, col = nchar(content[[end_line]]))
-                )
-            )
-        )
-    })
-
-    c(section_symbols, chunk_symbols)
-}
-
-get_document_section_symbols <- function(uri, document) {
-    if (document$is_rmarkdown) {
-        get_rmd_document_section_symbols(uri, document)
-    } else {
-        get_r_document_section_symbols(uri, document)
-    }
-}
-
 #' Get all the symbols in the document
 #' @keywords internal
 document_symbol_reply <- function(id, uri, workspace, document, capabilities) {
@@ -204,7 +53,31 @@ document_symbol_reply <- function(id, uri, workspace, document, capabilities) {
     result <- definition_symbols
 
     if (isTRUE(capabilities$hierarchicalDocumentSymbolSupport)) {
-        section_symbols <- get_document_section_symbols(uri, document)
+        sections <- get_document_sections(uri, document)
+        section_symbols <- lapply(sections, function(section) {
+            symbol_information(
+                name = section$name,
+                kind = switch(section$type,
+                    section = SymbolKind$String,
+                    subsection = SymbolKind$String,
+                    chunk = SymbolKind$Key
+                ),
+                location = list(
+                    uri = uri,
+                    range = range(
+                        start = document$to_lsp_position(
+                            row = section$start_line - 1,
+                            col = 0
+                        ),
+                        end = document$to_lsp_position(
+                            row = section$end_line - 1,
+                            col = nchar(document$line(section$end_line))
+                        )
+                    )
+                )
+            )
+        })
+
         result <- c(result, section_symbols)
     }
 
