5766a0995bd7b127576065d2a4e317eba83e7cf6
Kun Ren <renkun@outlook.com>
2020-07-01 20:39:06 +0800

    Add expr folding ranges

--- R/folding.R
+++ R/folding.R
@@ -7,22 +7,41 @@ FoldingRangeKind <- list(
 #' Get all the folding ranges in the document
 #' @keywords internal
 document_folding_range_reply <- function(id, uri, workspace, document) {
+    result <- NULL
+    
     parse_data <- workspace$get_parse_data(uri)
     if (is.null(parse_data) ||
         (!is.null(parse_data$version) && parse_data$version != document$version)) {
         return(NULL)
     }
 
-    result <- NULL
+    xdoc <- parse_data$xml_doc
+    if (!is.null(xdoc)) {
+        exprs <- xml_find_all(xdoc, "//expr[@line2 > @line1]")
+        logger$info("expr_folding_range: ", length(exprs))
+        expr_line1 <- as.integer(xml_attr(exprs, "line1"))
+        expr_line2 <- as.integer(xml_attr(exprs, "line2"))
+        expr_folding_ranges <- .mapply(function(line1, line2) {
+            list(
+                startLine = line1 - 1,
+                endLine = line2 - 1,
+                kind = FoldingRangeKind$Region
+            )
+        }, list(expr_line1, expr_line2), NULL)
+        result <- c(result, expr_folding_ranges)
+    }
+
     sections <- get_document_sections(uri, document, type = c("section", "chunk"))
-    section_folding_ranges <- lapply(sections, function(section) {
-        list(
-            startLine = section$start_line - 1,
-            endLine = section$end_line - 1,
-            kind = FoldingRangeKind$Region
-        )
-    })
-    result <- c(result, section_folding_ranges)
+    if (length(sections)) {
+        section_folding_ranges <- lapply(sections, function(section) {
+            list(
+                startLine = section$start_line - 1,
+                endLine = section$end_line - 1,
+                kind = FoldingRangeKind$Region
+            )
+        })
+        result <- c(result, section_folding_ranges)
+    }
 
     if (length(result) == 0) {
         Response$new(id)
