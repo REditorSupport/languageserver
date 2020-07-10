6db8edfb919f58de0dbabc60e1242182d49d26ac
Kun Ren <renkun@outlook.com>
2020-07-10 17:53:15 +0800

    Add test cases

--- tests/testthat/test-folding.R
+++ tests/testthat/test-folding.R
@@ -45,7 +45,9 @@ test_that("Document section folding range works", {
     ), defn_file)
 
     client %>% did_save(defn_file)
-    result <- client %>% respond_document_folding_range(defn_file)
+    result <- client %>%
+        respond_document_folding_range(defn_file) %>%
+        keep(~ .$kind == FoldingRangeKind$Region)
     result <- result[order(sapply(result, "[[", "startLine"))]
 
     expect_equal(length(result), 3)
@@ -53,6 +55,45 @@ test_that("Document section folding range works", {
     expect_equal(result[[1]]$endLine, 6)
     expect_equal(result[[2]]$startLine, 1)
     expect_equal(result[[2]]$endLine, 6)
+    expect_equal(result[[3]]$startLine, 7)
+    expect_equal(result[[3]]$endLine, 8)
+})
+
+test_that("Document comment folding range works", {
+    skip_on_cran()
+    client <- language_client(capabilities = list(
+        textDocument = list(
+            documentSymbol = list(
+                hierarchicalDocumentSymbolSupport = TRUE
+            )
+        )
+    ))
+
+    withr::local_tempfile(c("defn_file"), fileext = ".R")
+    writeLines(c(
+        "# test 1",
+        "# test 2",
+        "f <- function(x) {",
+        "  # step1",
+        "  # step1",
+        "  x + 1",
+        "}",
+        "# test 3",
+        "# test 4",
+        "g <- function(x) { x - 1 }"
+    ), defn_file)
+
+    client %>% did_save(defn_file)
+    result <- client %>%
+        respond_document_folding_range(defn_file) %>%
+        keep(~ .$kind == FoldingRangeKind$Comment)
+    result <- result[order(sapply(result, "[[", "startLine"))]
+
+    expect_equal(length(result), 3)
+    expect_equal(result[[1]]$startLine, 0)
+    expect_equal(result[[1]]$endLine, 1)
+    expect_equal(result[[2]]$startLine, 3)
+    expect_equal(result[[2]]$endLine, 4)
     expect_equal(result[[3]]$startLine, 7)
     expect_equal(result[[3]]$endLine, 8)
 })
