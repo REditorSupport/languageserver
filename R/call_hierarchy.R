d2830732ad712fb4d749b82dbf3de7ff42158b68
Kun Ren <renkun@outlook.com>
2021-01-26 20:27:08 +0800

    Update call_hierarchy

--- R/call_hierarchy.R
+++ R/call_hierarchy.R
@@ -2,14 +2,28 @@
 prepare_call_hierarchy_reply <- function(id, uri, workspace, document, point) {
 
   token <- document$detect_token(point)
+  defn <- definition_reply(NULL, uri, workspace, document, point)
   token_quote <- xml_single_quote(token$token)
-
-  result <- NULL
 
   logger$info("prepare_call_hierarchy_reply: ", list(
     uri = uri,
-    token = token
+    token = token,
+    defn = defn$result
   ))
+
+  result <- list()
+
+  if (length(defn$result)) {
+    result <- list(
+      list(
+        name = token$token,
+        kind =  SymbolKind$Function,
+        uri = defn$uri,
+        range = defn$range,
+        selectionRange = defn$range
+      )
+    )
+  }
 
   logger$info("prepare_call_hierarchy_reply: ", result)
 
