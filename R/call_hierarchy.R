88d368d49b1d429e85a8d5a079023b6162e494eb
Kun Ren <renkun@outlook.com>
2021-01-29 14:55:42 +0800

    Update call_hierarchy_incoming_calls_reply

--- R/call_hierarchy.R
+++ R/call_hierarchy.R
@@ -42,42 +42,93 @@ call_hierarchy_incoming_calls_reply <- function(id, workspace, item) {
   token_quote <- xml_single_quote(item$name)
   result <- list()
 
+  in_calls <- new.env()
+
   for (doc_uri in workspace$documents$keys()) {
     doc <- workspace$documents$get(doc_uri)
     xdoc <- workspace$get_parse_data(doc_uri)$xml_doc
     if (is.null(xdoc)) next
 
-    symbols <- xml_find_all(xdoc,
-      glue("//SYMBOL_FUNCTION_CALL[text() = '{token_quote}']", token_quote)
-    )
+    defns <- workspace$get_definitions_for_uri(doc_uri)
 
-    line1 <- as.integer(xml_attr(symbols, "line1"))
-    col1 <- as.integer(xml_attr(symbols, "col1"))
-    line2 <- as.integer(xml_attr(symbols, "line2"))
-    col2 <- as.integer(xml_attr(symbols, "col2"))
+    for (defn in defns) {
+      start_point <- doc$from_lsp_position(defn$range$start)
+      end_point <- doc$from_lsp_position(defn$range$end)
+      line1 <- start_point$row + 1
+      col1 <- start_point$col + 1
+      line2 <- end_point$row + 1
+      col2 <- end_point$col
 
-    for (i in seq_len(length(symbols))) {
-      symbol_point <- list(row = line1[[i]] - 1, col = col1[[i]])
-      symbol_defn <- definition_reply(NULL, doc_uri, workspace, doc, symbol_point)
-      if (identical(symbol_defn$result, item$data$definition)) {
-        result <- c(result, list(list(
-          uri = doc_uri,
-          range = range(
-            start = doc$to_lsp_position(
-              row = line1[[i]] - 1,
-              col = col1[[i]] - 1
+      symbols <- xml_find_all(xdoc,
+        glue("//*[(self::SYMBOL or self::SYMBOL_FUNCTION_CALL) and
+        ((@line1 = {line1} and @col1 >= {col1}) or @line1 > {line1}) and
+        ((@line2 = {line2} and @col2 <= {col2}) or @line2 < {line2}) and
+        text() = '{token_quote}']",
+          line1 = line1, col1 = col1, line2 = line2, col2 = col2,
+          token_quote = token_quote
+        )
+      )
+
+      if (length(symbols) == 0) {
+        next
+      }
+
+      defn_hash <- digest::digest(defn)
+
+      symbol_names <- xml_name(symbols)
+      symbol_text <- xml_text(symbols)
+      symbol_line1 <- as.integer(xml_attr(symbols, "line1"))
+      symbol_col1 <- as.integer(xml_attr(symbols, "col1"))
+      symbol_line2 <- as.integer(xml_attr(symbols, "line2"))
+      symbol_col2 <- as.integer(xml_attr(symbols, "col2"))
+
+      for (i in seq_along(symbols)) {
+        symbol_point <- list(row = symbol_line1[[i]] - 1, col = symbol_col1[[i]])
+        symbol_defn <- definition_reply(NULL, doc_uri, workspace, doc, symbol_point)$result
+        if (!identical(symbol_defn, item$data$definition)) {
+          next
+        }
+
+        if (is.null(in_calls[[defn_hash]])) {
+          in_calls[[defn_hash]] <- list(
+            to = list(
+              name = defn$name,
+              kind = SymbolKind$Function,
+              uri = doc_uri,
+              range = defn$range,
+              selectionRange = defn$range,
+              data = list(
+                definition = defn
+              )
             ),
-            end = doc$to_lsp_position(
-              row = line2[[i]] - 1,
-              col = col2[[i]]
+            fromRangess = list()
+          )
+        }
+
+        in_calls[[defn_hash]]$fromRanges <- c(
+          in_calls[[defn_hash]]$fromRanges,
+          list(
+            range(
+              start = doc$to_lsp_position(
+                row = symbol_line1[[i]] - 1,
+                col = symbol_col1[[i]] - 1
+              ),
+              end = doc$to_lsp_position(
+                row = symbol_line2[[i]] - 1,
+                col = symbol_col2[[i]]
+              )
             )
           )
-        )))
+        )
       }
     }
   }
 
-  Response$new(id)
+  result <- unname(as.list.environment(in_calls))
+
+  logger$info("call_hierarchy_incoming_calls_reply: ", result)
+
+  Response$new(id, result = result)
 }
 
 call_hierarchy_outgoing_calls_reply <- function(id, workspace, item) {
