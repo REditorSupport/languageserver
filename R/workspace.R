6444d4acf00c4c35cdc2c1379adebd7c1fded731
ManuelHentschel <53863351+ManuelHentschel@users.noreply.github.com>
2021-03-17 18:07:30 +0100

    Put spaces around all infix operators

--- R/workspace.R
+++ R/workspace.R
@@ -160,7 +160,7 @@ Workspace <- R6::R6Class("Workspace",
                         # Make header look prettier:
                         pattern <- "<table.*?<td>(.*?)\\s*{(.*?)}<\\/td>.*?<\\/table>\\n*<h2>\\s*(.*?)\\s*<\\/h2>"
                         replacement <- "<b>\\1</b> <i>{\\2}</i><br/>\\3<hr/>"
-                        html <- gsub(pattern, replacement, html, perl=TRUE)
+                        html <- gsub(pattern, replacement, html, perl = TRUE)
                         result <- html_to_markdown(html)
                     }
 
