6dabf12d5a349ee70a352946796ccd0141f8cee1
Kun Ren <renkun@outlook.com>
2021-07-01 16:51:47 +0800

    Handle workspace/didChangeWatchedFiles

--- R/handlers-workspace.R
+++ R/handlers-workspace.R
@@ -1,3 +1,9 @@
+FileChangeType <- list(
+    Created = 1,
+    Changed = 2,
+    Deleted = 3
+)
+
 #' `workspace/didChangeWorkspaceFolders` notification handler
 #'
 #' Handler to the `workspace/didChangeWorkspaceFolders` [Notification].
@@ -28,6 +34,43 @@ workspace_did_change_configuration <- function(self, params) {
 #' @noRd
 workspace_did_change_watched_files <- function(self, params) {
     logger$info("workspace_did_change_watched_files:", params)
+
+    # All open documents will be automatically handled by lsp requests.
+    # Only non-open documents in a package should be handled here.
+
+    project_root <- self$rootPath
+    if (length(project_root) && is_package(project_root)) {
+        source_dir <- file.path(project_root, "R")
+        for (file_event in params$changes) {
+            uri <- file_event$uri
+            path <- path_from_uri(uri)
+
+            if (dirname(path) != source_dir) {
+                next
+            }
+
+            if (self$workspace$documents$has(uri)) {
+                doc <- self$workspace$documents$get(uri)
+                if (doc$is_open) {
+                    # skip open documents
+                    next
+                }
+            }
+
+            type <- file_event$type
+
+            if (type == FileChangeType$Created || type == FileChangeType$Changed) {
+                logger$info("load", path)
+                doc <- Document$new(uri, NULL, stringi::stri_read_lines(path))
+                self$workspace$documents$set(uri, doc)
+                self$text_sync(uri, document = doc, parse = TRUE)
+            } else if (type == FileChangeType$Deleted) {
+                logger$info("remove", path)
+                self$workspace$documents$remove(uri)
+            }
+        }
+        self$workspace$update_loaded_packages()
+    }
 }
 
 #' `workspace/symbol` request handler
